b28d883bbbc90c8bab78dfba838e0b4d491a5c70
Dejan Milicic <dejan.milicic@gmail.com>
2023-02-28 23:00:26 +0100

    delete portfolio

--- RavenPortfolio/RavenPortfolio/Program.cs
+++ RavenPortfolio/RavenPortfolio/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.Diagnostics;
 using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Queries;
 using RavenPortfolio;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
@@ -111,6 +112,37 @@ class Program
         }
     }
 
+    public static async Task DeletePortfolio(string portfolioId)
+    {
+        using (var store = new DocumentStore
+               {
+                   Database = "portfolio",
+                   Urls = new[] { "http://127.0.0.1:8080" }
+               })
+        {
+            store.Initialize();
+
+            // here to force a request for RavenDB, nothing else. So the benchmark won't have to create
+            // the connection to the server, we can assume that this is already there
+            store.Maintenance.Send(new Raven.Client.Documents.Operations.GetStatisticsOperation());
+
+            Console.WriteLine("Starting deletion...");
+            var sp = Stopwatch.StartNew();
+            
+            var operation = await store
+                .Operations
+                .SendAsync(new DeleteByQueryOperation(new IndexQuery
+                {
+                    Query = $"from 'Portfolios' where startsWith(id(), '{portfolioId}/')"
+                }));
+
+            await operation.WaitForCompletionAsync(TimeSpan.FromSeconds(15));
+
+
+            Console.WriteLine($"Completed, Elapsed time: {sp.Elapsed}");
+        }
+    }
+
     static async Task Main(string[] args)
     {
         //await BulkInsert();
@@ -118,6 +150,7 @@ class Program
         //await GetPortfolioForDay("p1", 2005, 5, 5);
 
         //await GetPortfolioForMonth("p1", 2005, 5);
+        
+        //await DeletePortfolio("p1");
     }
-
 }
